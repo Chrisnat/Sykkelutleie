8392f8851eb6483469e888e232b4e676a2b892af
Christoffer <chrisnat@stud.ntnu.no>
2019-03-06 12:02:03 +0100

    Oppdatering index

--- srcSykkelutleie/index.js
+++ srcSykkelutleie/index.js
@@ -1,1 +1,6 @@
-console.log("hei");
+import * as React from "react";
+import { Component } from "react-simplified";
+import ReactDOM from "react-dom";
+import { NavLink, HashRouter, Route } from "react-router-dom";
+//import { Student, studentService } from './services';
+import { Card, List, Row, Column, NavBar, Button, Form } from "./widgets";
