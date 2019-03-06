7a451bfb872ed10650f93f9090111f0027d61c0a
Christoffer <chrisnat@stud.ntnu.no>
2019-03-06 11:11:01 +0100

    Opprettet filer

---
+++ srcSykkelutleie/mysql_connection.js
@@ -0,0 +1,32 @@
+// @flow
+
+import mysql from "mysql";
+
+let config = {
+  host: "mysql.stud.iie.ntnu.no",
+  user: "g_idri1005_03",
+  password: "5pl1Edaz",
+  database: "g_idri1005_03"
+};
+
+// Setup database server reconnection when server timeouts connection:
+export let connection = mysql.createConnection(config);
+
+function setup() {
+  // Connect to MySQL-server
+  connection.connect(error => {
+    if (error) console.error(error); // If error, show error in console and return from this function
+  });
+
+  // Add connection error handler
+  connection.on("error", (error: { code: string }) => {
+    if (error.code === "PROTOCOL_CONNECTION_LOST") {
+      // Reconnect if connection to server is lost
+      connection = mysql.createConnection(config);
+      setup();
+    } else {
+      console.error(error);
+    }
+  });
+}
+setup();
